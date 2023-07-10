68439056ae9784e251bb2e6b02f0044505429e77
Grant <gergely.daniel7@gmail.com>
2023-07-10 10:01:06 +0200

    Change button to blue

--- src/components/CheckButton.tsx
+++ src/components/CheckButton.tsx
@@ -4,7 +4,7 @@ const CheckButton: FunctionComponent = ({ children }) => {
     return (
         <button
             type='submit'
-            class='px-4 py-2 bg-red-500 rounded text-white text-xl'
+            class='px-4 py-2 bg-blue-500 rounded text-white text-xl'
         >
             {children}
         </button>
